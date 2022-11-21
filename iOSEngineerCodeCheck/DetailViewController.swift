7cc17798bd152a069609dbe0c6fe225a02bb78ac
maple-fm <fuma.deguchi@gmail.com>
2022-11-21 19:19:28 +0900

    ファイル名の変更と変数名の変更

---
+++ iOSEngineerCodeCheck/DetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 史 翔新 on 2020/04/21.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var ImageLabel: UIImageView!
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var LangLabel: UILabel!
+    @IBOutlet weak var StrsLabel: UILabel!
+    @IBOutlet weak var WchsLabel: UILabel!
+    @IBOutlet weak var Frkslabel: UILabel!
+    @IBOutlet weak var IsssLabel: UILabel!
+    
+    var searchVC: SearchViewController!
+        
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        let repository = searchVC.repository[searchVC.index]
+
+        LangLabel.text = "Written in \(repository["language"] as? String ?? "")"
+        StrsLabel.text = "\(repository["stargazers_count"] as? Int ?? 0) stars"
+        WchsLabel.text = "\(repository["wachers_count"] as? Int ?? 0) watchers"
+        Frkslabel.text = "\(repository["forks_count"] as? Int ?? 0) forks"
+        IsssLabel.text = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
+        getImage()
+
+    }
+    
+    func getImage(){
+        
+        let repo = searchVC.repository[searchVC.index]
+        
+        TitleLabel.text = repo["full_name"] as? String
+        
+        if let owner = repo["owner"] as? [String: Any] {
+            if let imgURL = owner["avatar_url"] as? String {
+                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
+                    let img = UIImage(data: data!)!
+                    DispatchQueue.main.async {
+                        self.ImageLabel.image = img
+                    }
+                }
+                .resume()
+            }
+        }
+    }
+}
--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  iOSEngineerCodeCheck
 //
 //  Created by 史 翔新 on 2020/04/20.
@@ -8,21 +8,21 @@
 
 import UIKit
 
-class ViewController: UITableViewController, UISearchBarDelegate {
+class SearchViewController: UITableViewController, UISearchBarDelegate {
 
-    @IBOutlet weak var SchBr: UISearchBar!
+    @IBOutlet weak var SearchBar: UISearchBar!
     
-    var repo: [[String: Any]]=[]
+    var repository: [[String: Any]]=[]
     var task: URLSessionTask?
     var word: String!
     var url: String!
-    var idx: Int!
+    var index: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        SchBr.text = "GitHubのリポジトリを検索できるよー"
-        SchBr.delegate = self
+        SearchBar.text = "GitHubのリポジトリを検索できるよー"
+        SearchBar.delegate = self
     }
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
@@ -44,7 +44,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
             task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
                 if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                     if let items = obj["items"] as? [[String: Any]] {
-                        self.repo = items
+                        self.repository = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
                         }
@@ -59,28 +59,28 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "Detail" {
-            let dtl = segue.destination as! ViewController2
-            dtl.vc1 = self
+            let detail = segue.destination as! DetailViewController
+            detail.searchVC = self
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repo.count
+        return repository.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell()
-        let rp = repo[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let repository = repository[indexPath.row]
+        cell.textLabel?.text = repository["full_name"] as? String ?? ""
+        cell.detailTextLabel?.text = repository["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
-        idx = indexPath.row
+        index = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
     }
     
