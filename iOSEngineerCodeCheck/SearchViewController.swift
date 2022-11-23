8efdc534904c4f727498587d5c53d0c2e8a6794b
maple-fm <fuma.deguchi@gmail.com>
2022-11-23 14:57:38 +0900

    強制アンラップや強制ダウンキャストを解消した

--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -10,18 +10,21 @@ import UIKit
 
 class SearchViewController: UITableViewController, UISearchBarDelegate {
 
-    @IBOutlet weak var SearchBar: UISearchBar!
+    @IBOutlet weak var SearchBar: UISearchBar?
     
     var repository: [[String: Any]]=[]
     var task: URLSessionTask?
-    var word: String!
-    var url: String!
-    var idx: Int!
+    private var word: String?
+    private var url: String?
+    var idx: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        SearchBar.text = "GitHubのリポジトリを検索できるよー"
-        SearchBar.delegate = self
+
+        if let SearchBar = self.SearchBar {
+            SearchBar.text = "GitHubのリポジトリを検索できるよー"
+            SearchBar.delegate = self
+        }
     }
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
@@ -31,19 +34,24 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        task?.cancel()
+        guard
+            let task = task
+        else { return }
+        task.cancel()
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
-        word = searchBar.text!
+
+        guard
+            let word = searchBar.text
+        else { return }
 
         if word.count == 0 {
             return
         }
 
-        url = "https://api.github.com/search/repositories?q=\(word!)"
-        task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+        url = "https://api.github.com/search/repositories?q=\(word)"
+        task = URLSession.shared.dataTask(with: URL(string: url ?? "")!) { (data, res, err) in
             if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                 if let items = obj["items"] as? [[String: Any]] {
                     self.repository = items
@@ -54,12 +62,17 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
             }
         }
         // NOTE: resume()を呼ばないとリストが更新されない
-        task?.resume()
+        if let task = task {
+            task.resume()
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Detail" {
-            let detail = segue.destination as! DetailViewController
+            guard
+                let detail = segue.destination as? DetailViewController
+            else { return }
+
             detail.searchVC = self
         }
     }
